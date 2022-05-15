93c1c0a9d15f7ab8545b806c51840125ee2fd37d
allochi <allochi@gmail.com>
2022-05-15 16:20:42 +0200

    refactor: extract zipFile

--- archive.go
+++ archive.go
@@ -2,15 +2,16 @@ package main
 
 import (
 	"archive/zip"
+	"fmt"
 	"io"
 	"io/ioutil"
+	"log"
 	"os"
 )
 
 // Archive a data structure that holds a set of unique files
 // and compile them into one zip file
 type Archive struct {
-	// file  *os.File
 	files map[File]struct{}
 }
 
@@ -21,15 +22,19 @@ func NewArchive(files []File) *Archive {
 	}
 
 	for _, file := range files {
-		archive.files[file] = struct{}{}
+		archive.Add(file)
 	}
 
 	return &archive
 }
 
 // Add add a file to the archive
-func (a *Archive) Add(file File) {
+func (a *Archive) Add(file File) error {
+	if !file.IsValid() {
+		return fmt.Errorf("failed to archive %s - invalid file", file)
+	}
 	a.files[file] = struct{}{}
+	return nil
 }
 
 func (a *Archive) Write() (int, error) {
@@ -37,27 +42,43 @@ func (a *Archive) Write() (int, error) {
 	if err != nil {
 		return 0, err
 	}
-	defer os.Remove(archive.Name())
+	// TODO: uncomment!!!
+	// defer os.Remove(archive.Name())
 
 	zipper := zip.NewWriter(archive)
 	defer zipper.Close()
 
 	for file := range a.files {
-		f, err := os.Open(file.String())
+		log.Printf("archiving %s...", file)
+
+		err = a.zipFile(zipper, file)
 		if err != nil {
-			return 0, nil
+			log.Printf("failed to archive %s - ", err)
+			continue
 		}
 
-		w, err := zipper.Create(file.String())
-		if err != nil {
-			return 0, nil
-		}
-		if _, err := io.Copy(w, f); err != nil {
-			return 0, nil
-		}
-
-		f.Close()
+		log.Printf("file %s archived", file)
 	}
 
 	return 0, nil
+}
+
+func (a *Archive) zipFile(zipper *zip.Writer, file File) error {
+	f, err := os.Open(file.String())
+	if err != nil {
+		return fmt.Errorf("can't open file")
+	}
+	defer f.Close()
+
+	w, err := zipper.Create(file.String())
+	if err != nil {
+		return fmt.Errorf("can't zip file")
+	}
+
+	_, err = io.Copy(w, f)
+	if err != nil {
+		return fmt.Errorf("can't write file to archive")
+	}
+
+	return nil
 }
